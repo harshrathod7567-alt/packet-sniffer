f8fb1ad4460c1f9c0efb052630d2b075952577b8
Harsh <harshrathod7567@gmail.com>
2026-08-22 18:50:48 +0530

    Create packet_sniffer.py

---
+++ packet_sniffer.py
@@ -0,0 +1,7 @@
+from scapy.all import sniff
+
+def process_packet(packet):
+    print(packet.summary())
+
+print("Starting packet sniffer... Press Ctrl+C to stop.")
+sniff(prn=process_packet, count=10)
